8f01867822bd0bb705e02bf8f92976d4bdbf35da
Imran Shah <is9149933764@gmail.com>
2022-10-25 18:40:11 +0530

    done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-800 text-sm p-4 text-center text-white">
+      <p>Copyright © 2022  <strong className="md:font-bold text-cyan-600">IMRAN SHAH</strong> All Rights reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
